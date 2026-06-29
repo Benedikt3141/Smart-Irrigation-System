b3d86740f23a0fa1bf39172160b3ee75acdc3452
benedikt314159 <benebon@web.de>
2026-06-29 20:02:49 +0200

    cleaned up button code

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@
 #include "SD.h"
 #include "SPI.h"
 #include "MQ2.h" // library: https://github.com/labay11/MQ-2-sensor-library <- Thank you so much!
+#include "Buttons.h" //the button code that would mess up the main code
 
 
 
@@ -23,50 +24,6 @@ MQ2 mq2(MQ2_SENSOR_PIN);
 TFT_eSPI tft = TFT_eSPI();
 Adafruit_NeoPixel leds(NUMBER_LEDS, LED_PIN, NEO_RGB + NEO_KHZ800);
 RTC_DS3231 rtc;
-
-// ***************************************** Button Interrupt *****************************************
-volatile bool buttonInterrupt = false;
-
-
-// Option declaration
-enum Button {
-    BUTTON_NONE,
-    BUTTON_1,
-    BUTTON_2,
-    BUTTON_3,
-    BUTTON_4,
-    BUTTON_UNKNOWN
-};
-
-
-Button decodeButton(int value) {
-    if (value > 3800) return BUTTON_NONE;
-
-    if (value > 0 && value < 10) {
-        return BUTTON_1;
-    }
-
-    if (value > 300 && value < 400) {
-        return BUTTON_2;
-    }
-
-    if (value > 800 && value < 900) {
-        return BUTTON_3;
-    }
-
-    if (value > 1100 && value < 1200) {
-        return BUTTON_4;
-    }
-
-    return BUTTON_UNKNOWN;
-}
-
-void IRAM_ATTR onButtonChange() {
-    buttonInterrupt = true;
-}
-
-
-// ***************************************** Actual Code *****************************************
 
 void setup() {
   Serial.begin(115200);
@@ -80,14 +37,14 @@ void setup() {
   analogReadResolution(12);
 
 
-  Wire.begin(); //Start I2C
-  mq2.begin(); //Start MQ2 Sensor
+  Wire.begin(); // Start I2C
+  mq2.begin(); // Start MQ2 Sensor
 
-  if (!rtc.begin()) {
+  if (!rtc.begin()) { // Start Clock
     Serial.println("Couldn't find RTC!");
   }
 
-  attachInterrupt(
+  attachInterrupt( // Start Button Interrupt
         digitalPinToInterrupt(BUTTONS),
         onButtonChange,
         CHANGE
@@ -95,39 +52,5 @@ void setup() {
 }
 
 void loop() {
-    Serial.println(analogRead(BUTTONS));
-    delay(1000);
-    if (buttonInterrupt) {
-        buttonInterrupt = false;
-
-        delay(25); // Entprellen
-
-        int value = analogRead(BUTTONS);
-        Button button = decodeButton(value);
-
-        Serial.print("ADC: ");
-        Serial.print(value);
-        Serial.print(" -> ");
-
-        switch (button) {
-            case BUTTON_NONE:
-                Serial.println("kein Button");
-                break;
-            case BUTTON_1:
-                Serial.println("Button 1");
-                break;
-            case BUTTON_2:
-                Serial.println("Button 2");
-                break;
-            case BUTTON_3:
-                Serial.println("Button 3");
-                break;
-            case BUTTON_4:
-                Serial.println("Button 4");
-                break;
-            default:
-                Serial.println("unbekannter Wert");
-                break;
-        }
-    }
+    testButtons();
 }
